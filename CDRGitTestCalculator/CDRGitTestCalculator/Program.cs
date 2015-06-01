945d6c13242beba24be44f9a476bed8979537d01
cruff <cdruff@yahoo.com>
2015-06-01 13:43:59 -0500

    test

---
+++ CDRGitTestCalculator/CDRGitTestCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDRGitTestCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1,3));
+            Console.ReadLine();
+        }
+
+        static int Add(int first, int second)
+        {
+            return first + second;
+        }
+        
+        
+    }
+}
