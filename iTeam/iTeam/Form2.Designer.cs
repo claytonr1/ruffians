a1b743cd3b95227018e02e353eac882ba33e546a
cruff <cdruff@yahoo.com>
2015-08-28 09:18:21 -0500

    Pushing test

---
+++ iTeam/iTeam/Form2.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace iTeam
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(28, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Test info";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(445, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 166);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form 2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ iTeam/iTeam/Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTeam
+{
+    public partial class Form2 : Form
+    {
+        //attempting to implement control drag and drop
+        //very much a work in progress
+        //do not use for school demo, need to research this a bit more
+
+        // movement for label now works
+
+        private Point mousedownlocation;
+        private Label target;
+        public Form2()
+        {
+            InitializeComponent();
+            this.panel1.DragDrop += panel1_DragDrop;
+            this.panel1.DragOver += panel1_DragOver;
+        }
+
+        void panel1_DragOver(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("Drag over works");
+        }
+
+        void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+            MessageBox.Show("This part worked");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                mousedownlocation = e.Location;
+            }
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                label1.Left = e.X + label1.Left - mousedownlocation.X;
+                label1.Top = e.Y + label1.Top - mousedownlocation.Y;
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (sender is Label)
+            {
+                target = (Label)sender;
+                panel1.Controls.Add(target);
+                target.Dock = DockStyle.Top;
+                //panel1.Refresh();
+            }
+        }
+
+
+    }
+}
