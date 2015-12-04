7d04c301dbc3bc10d49714bc5735af83c813b7e3
claytonr1 <cdruff@yahoo.com>
2015-12-04 09:37:06 -0600

    Added support for category in routing configuration

--- CDRSportsStore/CDRSportsStore/App_Start/RouteConfig.cs
+++ CDRSportsStore/CDRSportsStore/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace CDRSportsStore
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapPageRoute(null, "list/{category}/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
