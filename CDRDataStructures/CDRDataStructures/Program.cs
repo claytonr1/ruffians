df38274879e6cc007de934b625adba02262b4bba
cruff <cdruff@yahoo.com>
2016-01-01 15:33:47 -0600

    Initial commit of basic linked list functions

--- CDRDataStructures/CDRDataStructures/Program.cs
+++ CDRDataStructures/CDRDataStructures/Program.cs
@@ -8,8 +8,20 @@ namespace CDRDataStructures
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            // just test code here for node creation
+            Node head;
+            head = null;
+            head = new Node("Ruff",1,head);
+
+            Console.WriteLine(head.GetItem());
+            Console.WriteLine(head.GetCount());
+
+            
+            Console.ReadLine();
+
         }
     }
 }
